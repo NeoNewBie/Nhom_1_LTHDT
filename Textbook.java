e90392b852f539176f6c5ba811cac74c0825b8bb
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2025-10-12 23:41:46 +0700

    upload textbook

---
+++ Textbook.java
@@ -0,0 +1,32 @@
+public class Textbook extends Book{
+    String monHoc;
+    String capDo;
+
+
+    public Textbook(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, String monHoc, String capDo)
+    {
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        this.monHoc = monHoc;
+        this.capDo = capDo;
+ 
+    }
+
+     public String getMonHoc() { 
+        return monHoc; }
+    public void setMonHoc(String monHoc) { 
+        this.monHoc = monHoc; 
+    }
+
+    public String getCapDo() { 
+        return capDo;
+    }
+    public void setCapDo(String capDo) { 
+        this.capDo = capDo; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Môn học: " + monHoc + ", Cấp độ: " + capDo;
+    }
+}
+
+
