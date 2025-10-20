8429ae7174b25c6a892cee26d056756555a8a2cc
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2025-10-20 19:13:14 +0700

    update textbook

--- Textbook.java
+++ Textbook.java
@@ -2,15 +2,16 @@ public class Textbook extends Book{
     private String monHoc;
     private String capDo;
 
-    public Textbook(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, String monHoc, String capDo)
+    public Textbook(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong,double giaCoBan, String monHoc, String capDo)
     {
-        super(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong, giaCoBan);
         this.monHoc = monHoc;
         this.capDo = capDo;
     }
 
     public String getMonHoc() { 
-        return monHoc; }
+        return monHoc; 
+    }
     public void setMonHoc(String monHoc) { 
         this.monHoc = monHoc; 
     }
@@ -23,10 +24,15 @@ public class Textbook extends Book{
     }
 
     @Override
+    public double tinhGiaBan(){
+        return (giaCoBan + ((2025 - namXuatBan) * 5000));
+    }
+    @Override
     public String toString() {
-        return super.toString() + 
+        return "Sách giáo trình --> " + super.toString() + 
         "\nMôn học: " + monHoc + 
-        "\nCấp độ: " + capDo;
+        "\nCấp độ: " + capDo + 
+        "\nGiá bán: " + tinhGiaBan();
     }
 }
 
